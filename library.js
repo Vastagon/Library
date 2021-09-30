28ebb7cbea6a165b245e1272b6bb357d7443eafc
Vastagon <jacob.yancey29@yahoo.com>
2021-09-29 19:32:18 -0500

    Update library.js

--- library.js
+++ library.js
@@ -14,21 +14,35 @@ let hasReadButton;
 hasReadId.checked = false;
 let mySaved = [];
 let n = 0;
-//mySaved.push(localStorage.getItem("book"))
-//mySaved = JSON.parse(mySaved[i])
+let amount;
+let finished = false;
+
+
+if(localStorage.getItem(amount) == null || localStorage.getItem(amount) == 0){
+  amount = 1;
+}else{
+  amount = localStorage.getItem(amount);
+}
+while(i < amount && !finished){
+  mySaved.push(localStorage.getItem(i))
+  i++;
+}
+i = 0;
  
   localToOnlineStorage();
 
 
 
   function localToOnlineStorage(){
+    console.log("Length: " + mySaved.length)
+    let a = 0;
+    while(a < mySaved.length){
+      let newBook = new book(bookNameId.value, authorId.value, numberOfPagesId.value, hasReadId.value)
+      myLibrary.push(newBook)//Adds a new book object to to myLibrary    
+      addBookToLibrary();    
+      a++;  
+    }
 
-    console.log(mySaved)
-
-    let newBook = new book(bookNameId.value, authorId.value, numberOfPagesId.value, hasReadId.value)
-    myLibrary.push(newBook)//Adds a new book object to to myLibrary   
-    
-    addBookToLibrary();
   }
 
 hasReadId.addEventListener('click', function(){
@@ -47,6 +61,7 @@ function book(title, author, pageNumber, hasRead){
 }
 createNewBook.addEventListener('click', function(){
   form.style.display= "block"
+  finished = true;
   changeOpacity();
 });
 function changeOpacity(){
@@ -93,10 +108,20 @@ function addBookToLibrary() {//Adds book to the page
     removeButton = document.createElement('button')
     removeButton.className = "removeButton"
     removeButton.innerText = "Remove"
-
-    name.innerText ="Name: "+ myLibrary[i].title;
-    author.innerText ="Author: "+ myLibrary[i].author;
-    numberofPages.innerText ="Pages: "+ myLibrary[i].pageNumber;
+    if(!finished){
+      let e = 0;
+      console.log(myLibrary.length)
+      while(e < myLibrary.length){
+      name.innerText ="Name: "+ myLibrary[e].title;
+      author.innerText ="Author: "+ myLibrary[e].author;
+      numberofPages.innerText ="Pages: "+ myLibrary[e].pageNumber;
+        e++;
+      }
+    }else{
+      name.innerText ="Name: "+ myLibrary[i].title;
+      author.innerText ="Author: "+ myLibrary[i].author;
+      numberofPages.innerText ="Pages: "+ myLibrary[i].pageNumber;
+    }
     
 
     function hasReadisChecked(){
@@ -123,6 +148,7 @@ function addBookToLibrary() {//Adds book to the page
     //Remove Button
     removeButton.addEventListener('click', function(){
       this.parentElement.style.display = "none";
+      localStorage.removeItem(i);
     })
 
     //Read Button
@@ -135,19 +161,9 @@ function addBookToLibrary() {//Adds book to the page
         hasRead.textContent = "Has Read"
       }
     })
-    console.log(myLibrary[i].name)
-    //I think I do storage here
-    //localStorage.clear()
 
-    mySaved.push([//Adds to json file
-      {
-          "title": myLibrary[i].title,
-          "author": myLibrary[i].author,
-          "pageNumber":myLibrary[i].pageNumber,
-          "hasRead":myLibrary[i].hasRead.textContent
-      }
-  ])
-      localStorage.setItem("book",JSON.stringify(mySaved))
+      localStorage.setItem(i,JSON.stringify(mySaved))
+      localStorage.setItem("amount", i);
     i++;
 }
 
